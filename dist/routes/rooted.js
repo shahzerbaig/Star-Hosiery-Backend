d8c1010f81bafd9a13843e3d09a7ed4fee99e47a
Shahzer Baig <shahzer.baig.sb@gmail.com>
2025-02-06 13:47:54 +0530

    reset dist

--- dist/routes/rooted.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const rootRouter = (0, express_1.Router)();
-rootRouter.get("/", (req, res, next) => {
-    res.status(200).json({ message: "Hi this is Star Hosiery" });
-});
-exports.default = rootRouter;
-//# sourceMappingURL=rooted.js.map
--- dist/server.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const JWT_SECRET = "your_jwt_secret";
-//# sourceMappingURL=server.js.map
