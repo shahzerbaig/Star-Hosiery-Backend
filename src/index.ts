484bda14e37e2cadccbc90557ea7a4bdd7cd690f
Shahzer Baig <shahzer.baig.sb@gmail.com>
2025-02-06 13:51:20 +0530

    update:cors policy updated

--- src/index.ts
+++ src/index.ts
@@ -19,13 +19,10 @@ const app: Express = express();
 // Make sure to add these middleware before routes
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "https://malicc.store", // Allow only your frontend
+    credentials: true, // Allow cookies and authentication headers
+    methods: "GET,POST,PUT,DELETE,OPTIONS", // Specify allowed methods
+    allowedHeaders: "Content-Type,Authorization", // Specify allowed headers
   })
 );
 app.use(express.json());
